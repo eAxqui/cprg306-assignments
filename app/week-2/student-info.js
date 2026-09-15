c61e7a3151fc8d24ef7993367cadab57dd83e85d
jericacilo <eric.acilo@live.com>
2026-09-15 01:20:04 -0600

    student info js export component update

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,8 +1,8 @@
-export default function Page() { 
-return ( 
-<main> 
-<h2>Student Name: John Eric Acilo</h2>
-<Link href="https://github.com/eAxqui">John Eric's GitHub Page</Link>
-</main> 
-); 
-} 
+export default function StudentInfoComponent() {
+  return (
+    <div>
+        <h2>Student Name: John Eric Acilo</h2>
+        <Link href="https://github.com/eAxqui">John Eric's GitHub Page</Link>
+    </div>
+  );
+}
